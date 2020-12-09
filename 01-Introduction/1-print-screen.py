96c45626396e3ff16c9c76931162db8f4cd3a7e3
Sir General <nigomezcr@valinor.domain.org>
2020-12-09 13:43:09 -0500

    Test commit

---
+++ 01-Introduction/1-print-screen.py
@@ -0,0 +1,9 @@
+#Python code for printing to screen
+"""
+All here is just a commentary
+"""
+
+import numpy as np #To extract mathematical funtions as sqrt, sin, cos, etc
+
+print("Hello there")
+print(np.sqrt(np.pi))
