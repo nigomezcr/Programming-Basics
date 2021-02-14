c0c82707b8cb8a594382f057d381c6eb57e259bf
Nicolás <nigomezcr@unal.edu.co>
2021-02-14 17:23:48 -0500

    Some changes to vector dynamics

--- 07-Pointers/4-vector-dynamic.cpp
+++ 07-Pointers/4-vector-dynamic.cpp
@@ -8,27 +8,25 @@ int main(int argc, char **argv)
 {
     const int N = 81000000; // memory size: N*4 -> max 2048000
     data_t data(N);
-    std::cout << data[0] << std::endl;
 
-    // create a new block of size N/2
+    std::cout << "Size of the vector: " << data.size() << std::endl;
+    //this creates a new block of size N/2
     data.resize(N/2);
+    std::cout << "New size: " << data.size() << std::endl;
 
-    // copy the N/2 old elementscopiar in the new block
+    //if we don't know the numbers of elements we will need for data,  we reserve some
     data.reserve(1000);
 
-    // free memory of the olf block
+    // assing number to last element in the vector with push_back
     data.push_back(1);
+    std::cout << "After push_back" << std::endl;
+    std::cout << "Vector has size: " <<  data.size() << std::endl;
+    std::cout << "Value of last element: " <<data[N/2] << std::endl;
+
     data.push_back(10);
+    std::cout << "Vector has size: " <<  data.size() << std::endl;
+    std::cout << "Value of last element: " <<data[N/2+1] << std::endl;
 
-    // ... 998
-    data.push_back(20);
-    // pedir nueva memoria // 2*1000 = 2000
-    // copiar los datos viejos en la nueva memoria
-    // liberar la memoria vieja
-    data.push_back(30);
-
-    data.size(); // cuantos datos tengo guardados
-    data.capacity();
 
     return 0;
 }
